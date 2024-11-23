6bc69ca9238ad4a2fd8c8931578c41ccf1d16a01
정동준 <ehdwns7f@gmail.com>
2024-11-23 18:12:10 +0900

    기말과제 1

---
+++ 기말과제/기온출력코드.py
@@ -0,0 +1,27 @@
+import requests
+
+API_KEY = "6e24a98dde7aa7fbc22d29dd1294bac7"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+def get_weather(lat, lon):
+    params = {
+        "lat": lat,
+        "lon": lon,
+        "appid": API_KEY,
+        "units": "metric",
+        "lang": "kr"
+    }
+    response = requests.get(BASE_URL, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        current_temp = data["main"]["temp"]
+        weather_description = data["weather"][0]["description"]
+        print(f"현재 온도: {current_temp}°C, 날씨: {weather_description}")
+    else:
+        print(f"API 호출 실패: {response.status_code}, {response.text}")
+
+
+# 위도, 경도.
+latitude = 37.5665
+longitude = 126.9780
+get_weather(latitude, longitude)
